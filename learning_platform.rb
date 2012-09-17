1ba641c3574a64cd6a3b65d4c748685d9a941270
Emma Poirier <manuepoirier@gmail.com>
2012-09-17 21:37:12 +0100

    worked on the index

--- learning_platform.rb
+++ learning_platform.rb
@@ -1,4 +1,6 @@
 require 'sinatra'
+require 'active_support/inflector'
+require 'active_support/core_ext'
 
 get '/' do
   haml :index
@@ -7,4 +9,3 @@ end
 get '/learning_materials/:material_name' do
   haml params[:material_name].to_sym
 end
-  
