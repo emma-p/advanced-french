fb76c46c82c2656d5423fce4a634c8484d419759
Emma Poirier <manuepoirier@gmail.com>
2012-11-06 11:37:01 +0000

    finished the first exercise view template

--- views/helpers/exercise_helper.rb
+++ views/helpers/exercise_helper.rb
@@ -2,4 +2,13 @@ module ViewHelper
   def bold text
     "<b>#{text}</b>"
   end
+  def substitute_answer question, user_answer_number, hint
+    question.gsub('__', "<input type='form', name='#{user_answer_number}', value= ''></input> (#{bold(hint)})")
+  end
+  def wrong_answer string
+    "<p>#{string} <i class='icon-remove'></i></p>"
+  end
+  def good_answer string
+    "<p>#{string} <i class='icon-ok'></i></p>"
+  end
 end
