7f62a6f2de94d58ef73a2ce53078d6a47f9b10bc
Emma Poirier <manuepoirier@gmail.com>
2012-11-11 15:49:50 +0000

    add connection to mongo for production env

--- connection.rb
+++ connection.rb
@@ -1,7 +1,17 @@
 class Connection
-  @db = Mongo::Connection.new.db "learning_platform_db", safe: true
-
   def self.db
-    @db
+    @db ||= get_db
+  end
+
+  def self.get_db
+    if ENV['RACK_ENV'] == 'production'
+      db_info = URI.parse(ENV['MONGOHQ_URL'])
+      db_name = db_info.path.gsub(/^\//, '')
+      db = Mongo::Connection.new(db_info.host, db_info.port).db(db_name) 
+      db.authenticate(db_info.user, db_info.password) unless (db_info.user.nil? || db_info.user.nil?) 
+      db
+    else
+      db = Mongo::Connection.new.db "learning_platform_db", safe: true
+    end
   end
 end
