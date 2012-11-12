d0c1716ccca23838b4ff2994e96f00f31d57da0a
Emma Poirier <manuepoirier@gmail.com>
2012-11-12 11:06:22 +0000

    add new tests

--- spec/integration/categories_fetcher_spec.rb
+++ spec/integration/categories_fetcher_spec.rb
@@ -5,13 +5,6 @@ describe CategoriesFetcher do
   describe '#get_categories' do
     subject { CategoriesFetcher.new }
     it 'returns the list of lesson categories' do
-    #  subject.lesson_file_path = 'spec/data/lessons_test.json'
-    #  categories = subject.get_categories
-    #  orth = categories.find { |cat| cat.name == 'Orthographe' }
-
-    #  categories.map(&:name).should =~ ["Grammaire", "Orthographe"]
-    #  orth.lessons.size.should == 1
-    #  orth.lessons.first.title.should == 'Accents sur la lettre e'
     end
   end
 end
